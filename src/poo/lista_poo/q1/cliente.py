6202aca849babc581c5a063dbb86a9e6a88373e3
Anderson Soares <andersonsoares@ufra.edu.br>
2026-08-08 12:07:51 -0300

    Feat: Resolvendo a questão 06 da lista de POO

--- src/poo/lista_poo/q1/cliente.py
+++ src/poo/lista_poo/q1/cliente.py
@@ -1,10 +1,44 @@
 from pedido import Pedido
 
 class Cliente:
-    def __init__(self, nome: str, cpf: str = ""):
+    def __init__(self, 
+                 nome      : str, 
+                 cpf       : str = "",
+                 endereco  : str = "",
+                 cep       : str = "",
+                 categoria : str = "",
+                 sexo      : str = "f"
+                 ):
+        self.erro = ""
         self.nome = nome
         self.cpf = cpf
+        self.endereco = endereco
+        self.cep = cep
+        self.categoria = categoria
+        self.sexo = sexo        
         self.pedidos :list[Pedido] = []
+
+    @property
+    def nome(self) -> str:
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome:str):
+        if len(nome) == 0:
+            self.erro = 'O campo "nome" é obrigatório.'
+        else:
+            self.__nome = nome
+
+    @property
+    def cpf(self) -> str:
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf:str):
+        if len(cpf) == 0:
+            self.erro = 'O campo "cpf" é obrigatório.'
+        else:
+            self.__cpf = cpf
 
     def __str__(self):
         return f"{self.nome} - CPF: {self.cpf}"
---
+++ src/poo/lista_poo/q1/principal.py
@@ -0,0 +1,56 @@
+# pyuic6 -x gui.ui -o gui.py
+import sys
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from gui_qt_designer import Ui_MainWindow
+from cliente import Cliente
+
+class Principal(Ui_MainWindow, QMainWindow):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        super().setupUi(self)
+        self.frame_msg.hide()
+        self.lista_clientes = []
+        self.cor_sucesso = "background-color: rgb(209, 255, 209);"
+        self.cor_erro = "background-color: rgb(250, 185, 185);"
+
+        #### acoes dos botoes ###
+        self.pushButton_salvar_cliente.clicked.connect(
+            self.salvar_cliente
+            )
+        self.pushButton_fechar_msg.clicked.connect(
+            lambda: self.frame_msg.hide()
+            )
+        
+
+    def salvar_cliente(self):
+        nome = self.lineEdit_nome.text()
+        cpf  = self.lineEdit_cpf.text()
+
+        c = Cliente(nome, cpf)
+        if not c.erro: # se erro estiver com texto entao VERDADE
+            self.lista_clientes.append(c)
+            print("Cliente salvo com sucesso.")
+            self.frame_msg.show()
+            self.label_msg.setText("Cliente salvo.")
+            self.label_msg.setStyleSheet(self.cor_sucesso)
+        else:
+            self.frame_msg.show()
+            self.label_msg.setText(f"{c.erro}")
+            self.label_msg.setStyleSheet(self.cor_erro)
+
+    # Sobrescrevendo o evento de fechamento
+    def closeEvent(self, event):
+        print("\n--- Exibindo conteúdo da lista antes de fechar ---")
+        for item in self.lista_clientes:
+            print(item)
+        print("--------------------------------------------------\n")
+        
+        # IMPORTANTE: Aceita o evento e permite que a janela feche
+        event.accept()
+
+if __name__ == "__main__":
+    qt = QApplication(sys.argv)    
+    principal = Principal()
+    principal.show()
+    qt.exec()
